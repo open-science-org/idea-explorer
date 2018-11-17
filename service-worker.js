de0aebad2b27a6d8a6aa7db79da79e12c8b7aac5
Abinash Koirala <koirala.abinash@gmail.com>
2018-11-17 12:04:23 -0500

    Publish site 0.0.2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d9df2c39d34be8e394fc686c77e3790.js"
+  "./precache-manifest.8beaa071af0db63b08bd2a4cd02477f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
