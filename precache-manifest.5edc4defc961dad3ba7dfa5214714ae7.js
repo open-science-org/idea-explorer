cde5ff201487b8c854c17ab04b0665af1ca47d73
Abinash Koirala <koirala.abinash@gmail.com>
2018-11-18 18:25:35 -0500

    Build website

---
+++ precache-manifest.5edc4defc961dad3ba7dfa5214714ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "623b00547f2c0c0ce756",
+    "url": "./static/css/main.bd482d78.chunk.css"
+  },
+  {
+    "revision": "623b00547f2c0c0ce756",
+    "url": "./static/js/main.623b0054.chunk.js"
+  },
+  {
+    "revision": "8387619f77992ec18dd7",
+    "url": "./static/js/1.8387619f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2314381bf52721d32910ee91eba5e4b3",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c4ca8d55c153ee75778f879ddf79f87.js"
+  "./precache-manifest.5edc4defc961dad3ba7dfa5214714ae7.js"
 );
 
 workbox.clientsClaim();
